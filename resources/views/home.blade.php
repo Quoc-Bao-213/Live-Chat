6580b5ca511095e7772af83ff7d14e1b7e1098a9
Quoc-Bao-213 <55703442+Quoc-Bao-213@users.noreply.github.com>
2020-02-14 17:04:12 +0700

    npm install, auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="user-wrapper">
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">QuocBao</p>
+                                <p class="email">QuocBao@gmail.com</p>
+                            </div>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
